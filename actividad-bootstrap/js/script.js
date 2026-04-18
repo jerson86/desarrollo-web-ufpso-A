11fc88bdef7aef3d35c527f28d7a4604477352f4
Jerson garcia <jersonjavier@gmail.com>
2026-04-18 10:07:56 -0500

    cambios bootatrap grupo A

--- actividad-bootstrap/js/script.js
+++ actividad-bootstrap/js/script.js
@@ -1,33 +1,34 @@
-// FORMULARIO
-document.getElementById("loginForm").addEventListener("submit", function(e) {
-    e.preventDefault();
+// Botones de las cards para abrir una ventana modal
+let botones = document.querySelectorAll(".btn-modal");
 
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    let mensaje = document.getElementById("mensaje");
+botones.forEach(boton => {
+    boton.addEventListener("click", function(){
+        let modal = new bootstrap.Modal(document.getElementById("miModal"));
+        modal.show();
+    })
+})
+
+// validacion del formulario
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault()
+
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    let message = document.getElementById("message")
+    // Valores didacticos para esta actividad
     const USER_DEFAULT = "prueba@gmail.com"
     const PASSWORD_DEFAULT = "123456"
 
-    if (email === "" || password === "") {
-        mensaje.innerHTML = "Todos los campos son obligatorios";
-        mensaje.className = "text-warning";
-    } else if (email === USER_DEFAULT || password === PASSWORD_DEFAULT){
-        mensaje.innerHTML = "Bienvenido al sistema";
-        mensaje.className = "text-success";
+    if (email === "" || password === ""){
+        message.innerHTML = "Todos los campos son obligatorios!"
+        message.className = "text-warning"
+    } 
+    else if (email === USER_DEFAULT && password === PASSWORD_DEFAULT){
+        message.innerHTML = "Bienvenido al sistema"
+        message.className = "text-success"
     }
     else {
-        mensaje.innerHTML = "Error al procesar los datos";
-        mensaje.className = "text-danger";
+        message.innerHTML = "Error: Usuario o contrasena invalidas!"
+        message.className = "text-danger"
     }
-});
-
-
-// BOTONES DE CARDS → ABRIR MODAL
-let botones = document.querySelectorAll(".btn-info");
-
-botones.forEach(boton => {
-    boton.addEventListener("click", function() {
-        let modal = new bootstrap.Modal(document.getElementById("miModal"));
-        modal.show();
-    });
-});
+})
